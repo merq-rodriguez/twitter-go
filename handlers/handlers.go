9d717c0cf2f764133730764627e4c065286a0b99
Stiven Rodriguez <merq-rodriguez@udla.edu.co>
2021-01-19 16:41:50 -0500

    feat: add handlers for controllers

---
+++ handlers/handlers.go
@@ -0,0 +1,30 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/merq-rodriguez/twitter-clone-backend-go/middlewares"
+	"github.com/merq-rodriguez/twitter-clone-backend-go/modules/auth"
+	"github.com/rs/cors"
+)
+
+/*
+RunHandlers function: run handdlers with controllers enpoints
+*/
+func RunHandlers() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/signup", middlewares.CheckDB(auth.Signup)).Methods("POST")
+	router.HandleFunc("/singin", middlewares.CheckDB(auth.Signin)).Methods("POST")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	handler := cors.AllowAll().Handler(router)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+}
