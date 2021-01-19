0d8ba33acedaa4eb1d4e7d09cb43af06965ddcc0
Stiven Rodriguez <merq-rodriguez@udla.edu.co>
2021-01-19 16:36:33 -0500

    fat (users): add user struct module

---
+++ modules/users/models/user.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+/*
+User model struct for MongoDB
+*/
+type User struct {
+	ID        primitive.ObjectID `bson:"_id, omitempty" json:"id"`
+	Name      string             `bson:"name"      json:"name, omitempty"`
+	Lastname  string             `bson:"lastname"  json:"lastname, omitempty"`
+	BirthDate time.Time          `bson:"birthDate" json:"birthDate, omitempty"`
+	Email     string             `bson:"email"     json:"email"`
+	Password  string             `bson:"password"  json:"password, omitempty"`
+	Avatar    string             `bson:"avatar"    json:"avatar, omitempty"`
+	Banner    string             `bson:"banner"    json:"banner, omitempty"`
+	Location  string             `bson:"location"  json:"location, omitempty"`
+	Website   string             `bson:"website"   json:"website, omitempty"`
+}
