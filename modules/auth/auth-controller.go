bef8443c9a87660dfb7c75a249fbc8ea7ed8bf83
Stiven Rodriguez <merq-rodriguez@udla.edu.co>
2021-01-20 19:48:00 -0500

    refactor (): change paths

--- modules/auth/auth-controller.go
+++ modules/auth/auth-controller.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/merq-rodriguez/twitter-clone-backend-go/common/response/httpstatus"
-	jwtmodel "github.com/merq-rodriguez/twitter-clone-backend-go/modules/auth/models"
+	jwtResponse "github.com/merq-rodriguez/twitter-clone-backend-go/common/jwt/types"
+	HttpStatus "github.com/merq-rodriguez/twitter-clone-backend-go/common/response/http"
 	"github.com/merq-rodriguez/twitter-clone-backend-go/modules/users"
 	"github.com/merq-rodriguez/twitter-clone-backend-go/modules/users/models"
 
@@ -20,34 +20,34 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		http.Error(w, "Error receive data user: "+err.Error(), httpstatus.BAD_REQUEST)
+		http.Error(w, "Error receive data user: "+err.Error(), HttpStatus.BAD_REQUEST)
 		return
 	}
 
 	if len(user.Email) == 0 {
-		http.Error(w, "Email required", httpstatus.BAD_REQUEST)
+		http.Error(w, "Email required", HttpStatus.BAD_REQUEST)
 		return
 	}
 
 	if len(user.Password) < 8 {
-		http.Error(w, "Password length more of 8 characters", httpstatus.BAD_REQUEST)
+		http.Error(w, "Password length more of 8 characters", HttpStatus.BAD_REQUEST)
 		return
 	}
 
 	_, wanted, _ := users.UserAlreadyExist(user.Email)
 	if wanted == true {
-		http.Error(w, "A user already exists with this email", httpstatus.BAD_REQUEST)
+		http.Error(w, "A user already exists with this email", HttpStatus.BAD_REQUEST)
 		return
 	}
 
 	_, status, err := users.CreateUser(user)
 	if err != nil {
-		http.Error(w, "User not created: "+err.Error(), httpstatus.BAD_REQUEST)
+		http.Error(w, "User not created: "+err.Error(), HttpStatus.BAD_REQUEST)
 		return
 	}
 
 	if status == false {
-		http.Error(w, "No se ha logrado insertar el registro del usuario", httpstatus.BAD_REQUEST)
+		http.Error(w, "No se ha logrado insertar el registro del usuario", HttpStatus.BAD_REQUEST)
 		return
 	}
 
@@ -65,30 +65,30 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&user)
 
 	if err != nil {
-		http.Error(w, "Credentials Invalid "+err.Error(), httpstatus.UNAUTHORIZED)
+		http.Error(w, "Credentials Invalid "+err.Error(), HttpStatus.UNAUTHORIZED)
 		return
 	}
 
 	if len(user.Email) == 0 {
-		http.Error(w, "Email required", httpstatus.BAD_REQUEST)
+		http.Error(w, "Email required", HttpStatus.BAD_REQUEST)
 		return
 	}
 
 	document, exist := users.Signin(user.Email, user.Password)
 
 	if exist == false {
-		http.Error(w, "Email or password invalid", httpstatus.BAD_REQUEST)
+		http.Error(w, "Email or password invalid", HttpStatus.BAD_REQUEST)
 		return
 	}
 
 	jwtKey, err := jwt.CreateToken(document)
 	if err != nil {
-		http.Error(w, "Error creating token", httpstatus.INTERNAL_SERVER_ERROR)
+		http.Error(w, "Error creating token", HttpStatus.INTERNAL_SERVER_ERROR)
 		return
 	}
 
-	response := jwtmodel.JsonWebToken{
-		Token: jwtKey,
+	response := jwtResponse.JsonWebToken{
+		AccessToken: jwtKey,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
