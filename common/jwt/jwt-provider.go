52ae8ec786066bdb654c4a1dcde6c2936e5bd7b5
Stiven Rodriguez <merq-rodriguez@udla.edu.co>
2021-01-22 22:36:24 -0500

    feat (jwt): implement expires in with create jwt

--- common/jwt/jwt-provider.go
+++ common/jwt/jwt-provider.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	constant "github.com/merq-rodriguez/twitter-clone-backend-go/common/jwt/constants"
 	"github.com/merq-rodriguez/twitter-clone-backend-go/modules/users/models"
 )
 
@@ -11,13 +12,14 @@ import (
 CreateToken function for generate Json Web Token
 */
 func CreateToken(user models.User) (string, error) {
-	secretKey := []byte("MastersdelDesarrollo_grupodeFacebook")
+	var expiresin = time.Duration(constant.EXPIRES_IN)
+	secretKey := []byte(constant.SECRET_KEY)
 
 	payload := jwt.MapClaims{
 		"email": user.Email,
 		"name":  user.Name,
 		"_id":   user.ID.Hex(),
-		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+		"exp":   time.Now().Add(time.Hour * expiresin).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
