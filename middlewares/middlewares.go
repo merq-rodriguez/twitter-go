ab5f7bab06e0d76eac6be227ac1753bcfec0f483
Stiven Rodriguez <merq-rodriguez@udla.edu.co>
2021-01-22 22:42:44 -0500

    feat (): add comments and implement HttpStatus enum

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -4,7 +4,13 @@ import (
 	"net/http"
 
 	"github.com/merq-rodriguez/twitter-clone-backend-go/common/database"
+	HttpStatus "github.com/merq-rodriguez/twitter-clone-backend-go/common/response/http"
 )
+
+/*
+HandlerRequest type for manage request error
+*/
+type HandlerRequest func(http.ResponseWriter, *http.Request) error
 
 /*
 CheckDB middleware for show status of database
@@ -12,10 +18,26 @@ CheckDB middleware for show status of database
 func CheckDB(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if database.CheckConnection() == false {
-			http.Error(w, "Failed connection with database", 500)
+			http.Error(w, "Failed connection with database", HttpStatus.INTERNAL_SERVER_ERROR)
 			return
 		}
 
 		next.ServeHTTP(w, r)
 	}
+}
+
+func (fn HandlerRequest) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := fn(w, r) //Call handler function
+	if err == nil {
+		return
+	}
+
+	if database.CheckConnection() == false {
+		http.Error(w, "Failed connection with database", HttpStatus.INTERNAL_SERVER_ERROR)
+		return
+	}
+
+	fn.ServeHTTP(w, r)
+	//Error handling...
+
 }
