1c9fc7303bbab163591d941618900e6254911577
Stiven Rodriguez <merq-rodriguez@udla.edu.co>
2021-01-28 23:11:00 -0500

    feat: add execution context

---
+++ common/context/context.go
@@ -0,0 +1,24 @@
+package context
+
+import (
+	"context"
+	"log"
+	"net/http"
+)
+
+/*AddContext for add context to handlers routes */
+func AddContext(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		log.Println(r.Method, "-", r.RequestURI)
+		cookie, _ := r.Cookie("username")
+
+		if cookie != nil {
+			//Add data to context
+			ctx := context.WithValue(r.Context(), "Username", cookie.Value)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
