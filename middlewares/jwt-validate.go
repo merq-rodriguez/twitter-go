22673174bbd5f45e062d389f22255805de606406
Stiven Rodriguez <merq-rodriguez@udla.edu.co>
2021-01-28 23:13:38 -0500

    feat (): change name to function and add next serve http instruction

--- middlewares/jwt-validate.go
+++ middlewares/jwt-validate.go
@@ -1,22 +1,20 @@
 package middlewares
 
 import (
-	"fmt"
 	"net/http"
 
 	jwt "github.com/merq-rodriguez/twitter-clone-backend-go/modules/auth/jwt"
 )
 
-/*
-JWTValidate function for validate Json Web Token
-*/
-func JWTValidate(next http.HandlerFunc) http.HandlerFunc {
+/*AuthJWT function for validate Json Web Token*/
+func AuthJWT(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		token := r.Header.Get("Authorization")
-		fmt.Println(token)
-		_, _, _, err := jwt.ProcessToken(token)
+		_, _, err := jwt.ValidateToken(token)
 		if err != nil {
-			http.Error(w, "Error in token "+err.Error(), http.StatusBadRequest)
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
 		}
+		next.ServeHTTP(w, r)
 	}
 }
